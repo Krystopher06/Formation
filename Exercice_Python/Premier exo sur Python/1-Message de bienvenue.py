537d16fadaad3e2679b7d0c53681586da14db277
unknown <krystopher.logez@hotmail.fr>
2020-07-28 13:50:58 +0200

    Sauvegarde

---
+++ Exercice_Python/Premier exo sur Python/1-Message de bienvenue.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+nom= input("Tapez votre nom: ")
+print(" Bienvenue:" + nom) 
+
+
+
+
+
+a=input ("ton age ?")  # "input" est un "string"(une chaine de charactère )
+print(a*2)      # on fait un exemple avec "a*2" => Du coup le "a" (l'age donné) n'est pas considéré comme un nombre mais comme une chaine de charactère/
+                # Du coup, a*2 = 50 ans ( l'âge donné ) = 5050 est pas 100
+            
+             
---
+++ Exercice_Python/Premier exo sur Python/2-Saisir et calculer la somme de 2 nombres.py
@@ -0,0 +1,6 @@
+#- Ecrire un programme en Python qui demande à l'utilisateur de saisir deux nombres a et b et de lui afficher leur somme: a + b -
+
+a = input("Tapez la valeur de a:")
+b = input("Tapez la valeur de b:")
+somme = int(a) + int(b)
+print("la somme de a et de b est: " , somme)
